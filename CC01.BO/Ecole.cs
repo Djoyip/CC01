dd8ec88f769da5ebb41057410d54dfd094408de1
Jorelnj <61843424+Jorelnj@users.noreply.github.com>
2020-11-19 16:58:23 +0100

    Ajout de la BO

---
+++ CC01.BO/Ecole.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    public class Ecole
+    {
+        public string Nom { get; set; }
+        public string Adresse { get; set; }
+        public int Pb { get; set; }
+        public double Contact { get; set; }
+        public string Email { get; set; }
+
+        public Ecole()
+        {
+                
+        }
+
+        public Ecole(string nom, string adresse, int pb, double contact, string email)
+        {
+            Nom = nom;
+            Adresse = adresse;
+            Pb = pb;
+            Contact = contact;
+            Email = email;
+        }
+    }
+}
---
+++ CC01.BO/Etudiant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    public class Etudiant
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime Nee { get; set; }
+        public string A { get; set; }
+        public double Contact { get; set; }
+        public string Email { get; set; }
+
+        public Etudiant()
+        {
+                
+        }
+
+        public Etudiant(string nom, string prenom, DateTime nee, string a, double contact, string email)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Nee = nee;
+            A = a;
+            Contact = contact;
+            Email = email;
+        }
+    }
+}
